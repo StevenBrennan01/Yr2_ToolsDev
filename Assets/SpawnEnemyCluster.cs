9c45efec2f2d28f8f86f78ff3891ea42f21dc176
Steezyy17 <steveb1701@outlook.com>
2025-03-07 17:31:59 +0000

    Initial Commit

--- Assets/SpawnEnemyCluster.cs
+++ Assets/SpawnEnemyCluster.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class SpawnEnemyCluster : MonoBehaviour
 {
-    [Header("Preset Cluster Spawn")]
-    [Space(10)]
+    #region Title and Spacing
+    [Header("                                                     Enemy Cluster Spawner")]
+    [Space(15)]
+    #endregion
+
     [SerializeField]
     [Range(1, 15)]
     private int EnemiesToSpawn;
+
     [Space(10)]
 
     [SerializeField]
@@ -15,16 +19,13 @@ public class SpawnEnemyCluster : MonoBehaviour
 
     public void SpawnPresetCluster()
     {
+        //Button in editor script
         Debug.Log("Preset Enemies are spawned!!");
     }
 
     public void SpawnRandomCluster()
     {
+        //Button in editor script
         Debug.Log("Random Cluster of enemies spawned");
     }
-    
-
-    //button to spawn cluster with preset values
-
-    //button to spawn cluster with random values
 }
